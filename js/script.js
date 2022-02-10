34abfd192171479b7843a7ad75c11b544233303e
deveshbhardwaj970 <80453377+deveshbhardwaj970@users.noreply.github.com>
2022-02-10 11:08:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function signup(){
+    document.getElementById('login').style.display="none";
+    document.getElementById('signup').style.display="block";
+}
+function login(){
+    document.getElementById('signup').style.display="none";
+    document.getElementById('login').style.display="block";
+}
